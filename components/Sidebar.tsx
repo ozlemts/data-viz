1ecfcfe9dc69f8e945f8e9c78c64c437becb7bc6
ozlemts <ozlemturkerii@gmail.com>
2022-06-28 21:35:45 +0200

    Layout & Sidebar components are added

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const SideBar: NextPage = () => {
+  return (
+    <div className="w-80 bg-black text-white h-screen">
+      Sidebar
+    </div>
+  )
+}
+
+export default SideBar
