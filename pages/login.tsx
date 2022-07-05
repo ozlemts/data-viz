795d6b3f540eccd4fefccfec5dc51f76b669ed4b
ozlemts <ozlemturkerii@gmail.com>
2022-07-06 00:09:55 +0200

    next-auth & login page is added

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Layout from "../components/Layout";
+
+const LoginPage: NextPage = () => {
+  return (
+    <Layout>
+      Login
+    </Layout>
+  )
+}
+
+export default LoginPage
--- pages/the-expanse.tsx
+++ pages/the-expanse.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
 import Layout from "../components/Layout"
-import Orbit from "../components/charts/Orbit";
 
 const TheExpansePage: NextPage = () => {
   return (
     <Layout>
-      <Orbit/>
     </Layout>
   )
 }
